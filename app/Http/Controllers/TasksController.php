1b3f282b4c974432afa547046a86e78712210f3f
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-22 12:20:02 -0300

    Implementing addTask method

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -9,30 +9,32 @@ use Illuminate\Support\Facades\DB;
 class TasksController extends Controller
 {
     public function list($id) {
-        $task = DB::table('tasks')
-            ->select(['id', 'name', 'description'])
-            ->where('id', $id)
-            ->get();
-
-        if (count($task)) {
-            return response()->json($task)
+        $task = Task::firstWhere('id', $id);
+        if (!empty($task)) {
+            return response()
+                ->json($task)
                 ->header('Content-Type', 'application/json');
         } else {
-            return response()->json([
-                'code' => '404',
-                'message' => 'Task not found!',
-            ])
+            return response()
+                ->json([
+                    'code' => '404',
+                    'message' => 'Task not found!'
+                ])
                 ->header('Content-Type', 'application/json')
                 ->setStatusCode(404);
         }
     }
 
     public function listAll() {
-        return 'List all...';
+        return Task::all();
     }
 
-    public function addTask() {
-        return 'Add...';
+    public function addTask(Request $request) {
+        Task::create($request->all());
+        return response()
+            ->json(Task::firstWhere('name', $request->input('name')))
+            ->header('Content-Type', 'application/json')
+            ->setStatusCode(201);
     }
 
     public function editTask($id) {
