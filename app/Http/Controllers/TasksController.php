ef7e99483992421174182171f5015b98ef7cc328
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-22 00:54:40 -0300

    Implementing List method

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TasksController extends Controller
 {
+    public function list($id) {
+        $task = DB::table('tasks')
+            ->select(['id', 'name', 'description'])
+            ->where('id', $id)
+            ->get();
 
+        if (count($task)) {
+            return response()->json($task)
+                ->header('Content-Type', 'application/json');
+        } else {
+            return response()->json([
+                'code' => '404',
+                'message' => 'Task not found!',
+            ])
+                ->header('Content-Type', 'application/json')
+                ->setStatusCode(404);
+        }
+    }
+
+    public function listAll() {
+        return Task::all();
+    }
+
+    public function addTask(Request $request) {
+        $task = new Task;
+        $task->name = $request->input('name');
+        $task->description = $request->input('description');
+        $task->save();
+    }
+
+    public function editTask($id) {
+        return 'Edit '.$id;
+    }
+
+    public function removeTask($id) {
+        return 'Remove '.$id;
+    }
 }
