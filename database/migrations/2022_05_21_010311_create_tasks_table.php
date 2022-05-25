af8403c21ac29c1b3aa19ff22e967c2d98b9f360
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-24 22:46:58 -0300

    Renaming property

--- database/migrations/2022_05_21_010311_create_tasks_table.php
+++ database/migrations/2022_05_21_010311_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 30)->unique();
+            $table->string('title', 30)->unique();
             $table->string('description', 50);
             $table->softDeletes();
             $table->timestamps();
